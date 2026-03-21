d623a60b7ef6cf836aea0cbb5062f18904a39cef
arijitdatanai <arijitelectronicscomm@gmail.com>
2026-03-21 18:37:23 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [dataFlask, setDataFlask] = useState("");
+    const [question, setQuestion] = useState("");
+    
+    const handleChangeQuestion = (event) => {
+      setQuestion(event.target.value);
+    }
+
+    const handleSubmissionQuestion = () => {
+   
+        fetch(
+            'http://127.0.0.1:5001/nifty_agent',
+            {
+                method: 'POST',
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({ "question": question }),
+            }
+        )
+          .then((response) => response.json())
+          .then((dataFlask) => {
+              setDataFlask(dataFlask);
+            })
+          .catch((error) => {
+              console.error('Error:', error);
+          });
+
+  };
+
+    return (
+      <div className="content">
+          <h1>Nifty Agent</h1>
+          <p></p>
+      <div className="content">
+        <label><b>Ask Here: </b></label>
+        <p></p>
+        <label>
+          <textarea 
+            value={question}
+            onChange={handleChangeQuestion}
+            rows={5} cols={60} />
+        </label>
+        </div>
+        <p></p>
+        <button onClick={handleSubmissionQuestion}>Submit</button>
+        <p></p>
+        <p><b>{dataFlask["value"]}</b></p>
+      </div>)
+};
 
 export default App;
